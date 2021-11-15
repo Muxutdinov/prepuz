dd2980845dbd2d8ec903babde65d6406e3f2acdc
Jaxongir <jmuxutdinov@mail.ru>
2021-11-15 11:24:48 +0500

    updated

--- src/components/PartnersItem.jsx
+++ src/components/PartnersItem.jsx
@@ -16,7 +16,7 @@ const PartnersItem = () => {
       .get(
         "https://api.akpharm.uz/api/v1/manufacturer-list/hebu_medical/?lan=uz"
       )
-      .then((res) => setThird(Object.assign(res.data.logo.split())))
+      .then((res) => setThird(res.data))
       .catch((err) => console.log(err));
   };
   const getThirds = () => {
@@ -25,7 +25,6 @@ const PartnersItem = () => {
         "https://api.akpharm.uz/api/v1/drug-list/?page=1&manufacturer=hebu_medical&lan=uz"
       )
       .then((res) => setThirds(res.data.results))
-      // .then((res) => console.log(res.data.results))
       .catch((err) => console.log(err));
   };
   return (
@@ -39,38 +38,34 @@ const PartnersItem = () => {
             <img src={icon} />
             <div className="item3">HEBU MEDICAL</div>
           </div>
-          {third.map((value, index) => {
-            return (
-              <div className="Body">
-                <div className="left">
-                  <div className="Cards" key={index}>
-                    <img src={value} className="CardImg" />
-                  </div>
-                </div>
-                <div className="right">
-                  {/* <div className="Title">{value}</div> */}
-                  {/* <div className="Text">{value}</div> */}
-                </div>
+          <div className="Body">
+            <div className="left">
+              <div className="Cards">
+                <img src={third.logo} alt="Rasm" className="CardImg" />
               </div>
-            );
-          })}
+            </div>
+            <div className="right">
+              <div className="Title">{third.name}</div>
+              {/* <div className="Text">{value}</div> */}
+            </div>
+          </div>
           <div className="BtnTitle">Kompaniya mahsulotlari</div>
           <div className="ButtonWrapper">
-          {thirds.map((value, index) => {
-            return (
-              <>
+            {thirds.map((value, index) => {
+              return (
+                <>
                   <div className="BtnCardWrapper">
                     <div className="BtnCard">
                       <div className="ImgWrapper">
                         <img src={value.image} className="CardImgs" />
                       </div>
-                      <div>{value.slug}</div>
-                      <div>{value.name}</div>
+                      <div className="BtnTextCard">{value.slug}</div>
+                      <div className="BtnTitleCard">{value.name}</div>
                     </div>
                   </div>
-              </>
-            );
-          })}
+                </>
+              );
+            })}
           </div>
         </div>
       </div>
